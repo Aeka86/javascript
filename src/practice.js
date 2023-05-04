e326485ca457cfc71b5b4558e47727490a133ae4
Aigerim Sagimbayeva <aeka86@aol.com>
2023-05-04 15:12:08 +0600

    one way of doing a dropdown menu

--- src/practice.js
+++ src/practice.js
@@ -1,8 +1,17 @@
-const dropDown = document.querySelector(".dropbtn")
+const dropDownBtn = document.querySelector(".dropbtn")
 
-const dropDownBtn = document.querySelector(".dropdown-content")
+const dropDown = document.querySelector(".dropdown")
 
+dropDownBtn.addEventListener("click", function() {
+    dropDown.classList.toggle("clicked")
+  });
 
+  window.addEventListener("click", function(event)
+  {
+    if (!event.target.matches(".dropbtn")) {
+        dropDown.classList.remove("clicked")
+    }
+  }
+  )
 
-
-console.log(dropdown)
+console.log(dropDown)
